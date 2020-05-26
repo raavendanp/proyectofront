35eee82884f71f27a912f81b88b446d1edfd3251
unknown <raavendanp@eafit.edu.co>
2020-05-26 16:41:03 -0500

    set

--- proyectoFrontEnd/settings.py
+++ proyectoFrontEnd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j8s^1@dwpfb%=q$d*zazx495l%)vy6-i6@xybnkkpg(zm$_wiq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
